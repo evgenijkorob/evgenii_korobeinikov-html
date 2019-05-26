b7668abe3d45b2a33cf619a12ffb4cede81d2236
evgenijkorob <evgenij.k.wie@gmail.com>
2019-05-26 17:51:58 +0300

    feat(task11): add sorting

--- task11/src/app/journal/journal.component.ts
+++ task11/src/app/journal/journal.component.ts
@@ -2,6 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { StorageJournal, IStorageJournalRecord } from "../_model/journal";
 import { ProductProvidingService } from "../_service/product-providing.service";
 
+enum SortBy {
+  Tag,
+  Name,
+  Price,
+  Amount
+};
+
+enum SortType {
+  Descending = -1,
+  Ascending = 1
+};
+
 @Component({
   selector: 'app-journal',
   templateUrl: './journal.component.html',
@@ -10,6 +22,11 @@ import { ProductProvidingService } from "../_service/product-providing.service";
 export class JournalComponent implements OnInit {
 
   public records: IStorageJournalRecord[];
+  public readonly SortBy = SortBy;
+  public sortingBy: SortBy;
+  public readonly SortType = SortType;
+  public sortingType: SortType;
+
   private _journal: StorageJournal;
 
   constructor(
@@ -20,6 +37,42 @@ export class JournalComponent implements OnInit {
     this._journal = new StorageJournal();
     this._productProvider.fetchDataToJournal(this._journal);
     this.records = this._journal.records;
+  }
+
+  sort(by: SortBy): void {
+    this._resolveSortWay(by);
+    this.records = this.records.sort((a, b) => {
+      let result: number;
+      switch(this.sortingBy) {
+        case this.SortBy.Tag:
+          result = a.product.tag - b.product.tag;
+          break;
+        case this.SortBy.Name:
+          let collator = Intl.Collator();
+          result = collator.compare(a.product.name, b.product.name);
+          break;
+        case this.SortBy.Price:
+          result = a.product.price - b.product.price;
+          break;
+        case this.SortBy.Amount:
+          result = a.product.amount - b.product.amount;
+          break;
+        default:
+          result = 0;
+      }
+      result *= this.sortingType;
+      return result;
+    });
+  }
+
+  private _resolveSortWay(by: SortBy): void {
+    if (!this.sortingType || by !== this.sortingBy) {
+      this.sortingType = this.SortType.Descending;
+    }
+    else {
+      this.sortingType *= -1;
+    }
+    this.sortingBy = by;
   }
 
 }
